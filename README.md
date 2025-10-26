913ff54002b16a375bbd83d2e7f31671c0d6a55e
Emmanuel Pale Molina <epalemolina@gmail.com>
2025-10-26 17:23:00 -0600

    Productos controller added

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # E-Commerce Backend
 The purpose of the backend service is to offer business logic and data persistence for a E-Commerce app, applying best security practices.
 
-## Endpoints
-The next table shows all available endpoints
+## API
+In this section, you will find the available endpoints with their important details.
 
-|URI                 |HTTP Method|Description            |Role                   |Tested|
-|--------------------|-----------|-----------------------|-----------------------|------|
-|api/categorias      |GET        |Get all categories     |Administrador, Usuario |  ❌  |
-|api/categorias/{id} |GET        |Get a category by ID   |Administrador, Usuario |  ❌  |
-|api/categorias      |POST       |Create a new category  |Administrador          |  ❌  |
-|api/categorias/{id} |PUT        |Update a category by ID|Administrador          |  ❌  |
-|api/categorias/{id} |DELETE     |Delete a category by ID|Administrador          |  ❌  |
-||||||
-||||||
+Reference of roles;
+- A: Admin
+- U: User
+
+|URI                 |HTTP Method|Description            |Role |Tested|
+|--------------------|-----------|-----------------------|-----|------|
+|**CATEGORIAS**|
+|api/categorias      |GET        |Get all categories     |A, U |  ❌  |
+|api/categorias/{id} |GET        |Get a category by ID   |A, U |  ❌  |
+|api/categorias      |POST       |Create a new category  |A    |  ❌  |
+|api/categorias/{id} |PUT        |Update a category by ID|A    |  ❌  |
+|api/categorias/{id} |DELETE     |Delete a category by ID|A    |  ❌  |
+|**PRODUCTOS**|
+|api/productos      |GET        |Get all products        |A, U |  ❌  |
+|api/productos/{id} |GET        |Get a product by ID     |A, U |  ❌  |
+|api/productos      |POST       |Create a new product    |A    |  ❌  |
+|api/productos/{id} |PUT        |Update a product by ID  |A    |  ❌  |
+|api/productos/{id} |DELETE     |Delete a product by ID  |A    |  ❌  |
+|api/productos/{id}/categoria|GET |??   | - |  ❌  |
+|api/productos/{id}/categoria/{id}|DELETE|?? | - |  ❌  |
---
+++ controllers/productos.controller.js
@@ -0,0 +1,185 @@
+const { producto, categoria, Sequelize } = require('../models')
+const { body, validationResult } = require('express-validator')
+const Op = Sequelize.Op
+
+let self = {}
+
+self.productoValidator = [
+    body('titulo', 'El campo {0} es obligatorio').not().isEmpty(),
+    body('descripcion', 'El campo {0} es obligatorio').not().isEmpty(),
+    body('precio', 'El campo {0} es obligatorio').not().isEmpty().isDecimal({ force_decimal: false }),
+]
+
+//GET: api/productos
+self.getAll = async function (req, res, next) {
+    const { s } = req.query
+    const filters = {}
+    
+    if (s) {
+        filters.titulo = {
+            [Op.like]: `%${s}%`
+        }
+    }
+    
+    let data
+    try {
+        data = await producto.findAll({
+            where: filters,
+            attributes: [['id', 'productoId'], 'titulo', 'descripcion', 'precio', 'archivoid'],
+            include: {
+                model: categoria,
+                as: 'categorias',
+                attributes: [['id', 'categoriasId'], 'nombre', 'protegida'],
+                through: { attributes: []}
+            },
+            subQuery: false
+        })
+    } catch (error) {
+        return next(error)
+    }
+
+    return res.status(200).json(data)
+}
+
+//GET: api/productos/{id}
+self.get = async function (req, res, next) {
+    let id = req.params.id
+    let data = null
+
+    try {
+        data = await producto.findByPk(id, {
+            attributes: [['id', 'productoId'], 'titulo', 'descripcion', 'precio', 'archivoid'],
+            include: {
+                model: categoria,
+                as: 'categorias',
+                attributes: [['id', 'categoriaId'], 'nombre', 'protegida'],
+                through: { attributes: [] }
+            }
+        })
+    } catch (error) {
+        return next(error)
+    }
+
+    if(data) {
+        res.status(200).json(data)
+    } else {
+        res.status(404).send()
+    }
+}
+
+//POST: api/productos
+self.create = async function (req, res, next) {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        throw new Error(JSON.stringify(errors));
+    }
+    
+    let data
+    try {
+        data = await producto.create({
+            titulo: req.body.titulo,
+            descripcion: req.body.descripcion,
+            precio: req.body.precio,
+            archivoid: req.body.archivoid || null
+        })
+    } catch (error) {
+        return next(error)
+    }
+
+    req.bitacora('producto.crear', data.id)
+    res.status(201).json(data)
+}
+
+//PUT: api/productos/{id}
+self.update = async function (req, res, next) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        throw new Error(JSON.stringify(errors))
+    }
+
+    let id = req.params.id
+    let body = req.body
+    let data
+    try {
+        data = await producto.update(body, { where: {id: id} })
+    } catch (error) {
+        return next(error)
+    }
+
+    if(data[0] === 0) {
+        return res.status(404).send()
+    }
+
+    req.bitacora('producto.editar', id)
+    res.status(204).send()
+}
+
+//DELETE: api/productos/{id}
+self.delete = async function (req, res, next) {
+    let id = req.params.id
+    let data = null
+   
+    try {
+        data = await producto.findByPk(id)
+        if (data) {
+            data = await producto.destroy({ where: { id: id } })
+        }
+    } catch (error) {
+        return next(error)
+    }
+
+    if (data === 1) {
+        req.bitacora('producto.eliminar', id)
+        res.status(204).send()
+    } else {
+        res.status(404).send()
+    }
+}
+
+//POST: api/productos/{id}/categoria
+self.asignaCategoria = async function (req, res, next) {
+    let itemToAssign = null
+    let item = null
+
+    try {
+        itemToAssign = await categoria.findByPk(req.body.categoriaid)
+        item = await producto.findByPk(req.params.id)
+        if (item && itemToAssign) {
+            await item.addCategoria(itemToAssign)
+        }
+    } catch (error) {
+        return next(error)
+    }
+
+    if (!itemToAssign || !item) {
+        res.status(404).send()
+    } else {
+        req.bitacora('productocategoria.agregar', `${req.params.id}:${req.body.categoriaid}`)
+        res.status(204).send()
+    }
+}
+
+//DELETE: api/prodcutos/{id}/categoria/{id}
+self.eliminaCategoria = async function (req, res, next) {
+    let itemToRemove = null
+    let item = null
+
+    try {
+        itemToRemove = await categoria.findByPk(req.params.categoriaid)
+        item = await producto.findByPk(req.params.id)
+        if (itemToRemove && item) {
+            await item.removeCategoria(itemToRemove)
+        }
+    } catch (error) {
+        return next(error)
+    }
+
+    if (!itemToRemove || !item) {
+        res.status(404).send()
+    } else {
+        req.bitacora('productocategoria.remover', `${req.params.id}:${req.params.categoriaid}`)
+        res.status(204).send()
+    }
+}
+
+module.exports = self
